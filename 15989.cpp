6491570857d31cdabfd2a5a36213d30cd1a80df8
panggin <54972799+panggin@users.noreply.github.com>
2024-01-03 21:22:41 +0900

    modify comments

--- 15989.cpp
+++ 15989.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 const int MAX_N = 10000 + 1;
 
-int dp[MAX_N][4] = {0,}; // dp[현재숫자][사용된 숫자의 최솟값]
+int dp[MAX_N][4] = {0,}; // dp[총합][사용된 숫자의 최솟값]
 int t, n;
 
 void makeDPtable()
@@ -13,13 +13,13 @@ void makeDPtable()
     dp[3][3] = 1;
 
     for (n=1; n<MAX_N; n++) {
-        if (n-1 > 0) {
+        if (n-1 > 0) { // dp[n-1]의 경우에 +1이 되는 경우
             dp[n][1] = dp[n-1][1] + dp[n-1][2] + dp[n-1][3];
         } 
-        if (n-2 > 0) {
+        if (n-2 > 0) { // dp[n-2]의 경우에 +2가 되는 경우
             dp[n][2] = dp[n-2][2] + dp[n-2][3];
         } 
-        if (n-3 > 0) {
+        if (n-3 > 0) { // dp[n-3]의 경우에 +3이 되는 경우
             dp[n][3] = dp[n-3][3];
         }
     }
